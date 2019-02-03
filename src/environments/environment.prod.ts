bc1ac842d8c5ae146ab504589d4c791a137759b6
Alexander Burgett <4073978+aburgett87@users.noreply.github.com>
2019-02-03 19:54:57 +1100

    adding production environment config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  clientID: 'bM0gjqhJMcRsVjuDMM6x5mYoiuJuQaeb',
+  domain: 'speedtest-stubias.au.auth0.com',
+  callbackURL: 'http://speedtest.burgett.id.au/callback',
+  scope: 'read:testresults write:testresults',
+  audience: 'https://8u3rqd5cp8.execute-api.ap-southeast-2.amazonaws.com/Staging'
 };
