65e44b7a4f6046b718aaf2f651dac65cb209844f
Alexander Burgett <4073978+aburgett87@users.noreply.github.com>
2019-02-11 21:24:48 +1100

    chaning prod environment to staging

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   clientID: 'bM0gjqhJMcRsVjuDMM6x5mYoiuJuQaeb',
   domain: 'speedtest-stubias.au.auth0.com',
-  callbackURL: 'http://speedtest.burgett.id.au/callback',
+  callbackURL: 'https://staging.speedtest.burgett.id.au/callback',
   scope: 'read:testresults write:testresults',
   audience: 'https://8u3rqd5cp8.execute-api.ap-southeast-2.amazonaws.com/Staging'
 };
